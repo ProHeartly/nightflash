36837cce7f503689d65a15365f1ed5b6c2a58794
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-08-08 10:27:43 +0545

    initial

---
+++ content.js
@@ -0,0 +1,9 @@
+
+const canvas = document.createElement('canvas');
+
+canvas.id = 'nf-canvas';
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+document.body.appendChild(canvas);
